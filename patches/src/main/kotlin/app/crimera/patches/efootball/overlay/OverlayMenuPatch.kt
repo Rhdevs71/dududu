9d0eee8bc4904d4c2af8c385338fa3e4d9f81760
RH7155 <drahmanhakim71@gmail.com>
2026-09-18 17:41:11 +0700

    fix(efootball): use addInstructions without trailing label in OverlayMenuPatch

--- patches/src/main/kotlin/app/crimera/patches/efootball/overlay/OverlayMenuPatch.kt
+++ patches/src/main/kotlin/app/crimera/patches/efootball/overlay/OverlayMenuPatch.kt
@@ -10,7 +10,7 @@ import app.crimera.patches.efootball.misc.extension.efootballExtensionPatch
 import app.crimera.patches.efootball.utils.Constants.COMPATIBILITY_EFOOTBALL
 import app.crimera.patches.efootball.utils.Constants.EFB_OVERLAY_MANAGER_CLASS
 import app.crimera.patches.efootball.utils.Constants.GAME_ACTIVITY_CLASS
-import app.morphe.patcher.extensions.InstructionExtensions.addInstructionsWithLabels
+import app.morphe.patcher.extensions.InstructionExtensions.addInstructions
 import app.morphe.patcher.extensions.InstructionExtensions.instructions
 import app.morphe.patcher.patch.bytecodePatch
 import com.android.tools.smali.dexlib2.Opcode
@@ -33,19 +33,17 @@ val overlayMenuPatch =
                 if (onCreateMethod != null) {
                     val returnIndex = onCreateMethod.instructions.indexOfLast { it.opcode == Opcode.RETURN_VOID }
                     if (returnIndex >= 0) {
-                        onCreateMethod.addInstructionsWithLabels(
+                        onCreateMethod.addInstructions(
                             returnIndex,
                             """
                             sget-object v0, $GAME_ACTIVITY_CLASS->_activity:$GAME_ACTIVITY_CLASS
-                            if-eqz v0, :cond_efb_init
                             invoke-static {v0}, $EFB_OVERLAY_MANAGER_CLASS->init(Landroid/app/Activity;)V
-                            :cond_efb_init
                             """.trimIndent(),
                         )
                     }
                 }
             } catch (e: Exception) {
-                println("[OverlayMenuPatch] Warning: Failed to hook GameActivity.onCreate: ${e.message}")
+                println("[OverlayMenuPatch] Error hooking GameActivity.onCreate: ${e.message}")
             }
         }
     }
